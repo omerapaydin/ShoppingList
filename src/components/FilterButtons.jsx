43d053a5eea7aeb4a510853ce39273e108ed7864
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-22 23:50:31 +0300

    FilterButtons

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,10 +1,10 @@
 export default function FilterButtons() {
   return (
     <div className="filter-buttons border rounded p-3 mb-3">
-      <button item-filter="all" className="btn btn-primary">
+      <button item-filter="all" className="btn btn-primary me-1">
         All
       </button>
-      <button item-filter="incomplete" className="btn btn-secondary">
+      <button item-filter="incomplete" className="btn btn-secondary me-1">
         Incomplete
       </button>
       <button item-filter="completed" className="btn btn-secondary">
