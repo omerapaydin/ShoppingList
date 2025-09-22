a5ceea5c8d9285b489ef31cd19f742e72976db12
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-22 22:10:56 +0300

    NoItems

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -3,9 +3,11 @@ import Item from "./Item";
 export default function ListItems({ urunler }) {
   return (
     <ul className="shopping-list list-unstyled">
-      {urunler.map((i, index) => (
-        <Item key={index} urun={i} />
-      ))}
+      {urunler.length > 0 ? (
+        urunler.map((i, index) => <Item key={index} urun={i} />)
+      ) : (
+        <NoItems />
+      )}
     </ul>
   );
 }
