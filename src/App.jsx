d5dc6d4b511f468be3f8c27b4a373429e2549d75
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-22 21:26:53 +0300

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "./index.css";
+import Header from "./components/Header";
+import AddItemForm from "./components/AddItemForm";
+import NoItem from "./components/NoItem";
+import FilterButtons from "./components/FilterButtons";
+import ListItems from "./components/ListItems";
+import ClearButton from "./components/ClearButton";
 function App() {
   <div className="container">
     <Header />
